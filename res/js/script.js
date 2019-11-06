66e3af2380ad1bc8cb8f270eb085a1491148c820
Hemaharinii <rhemaharinii@gmail.com>
2019-11-06 22:59:25 +0530

    Movies rendition added
    Movies rendition added

--- res/js/script.js
+++ res/js/script.js
@@ -14,7 +14,53 @@ function loadMovies() {
       return res.json();
     })
     .then(function(result) {
-      console.log(result);
+      if (result.Response === "True" && result.Search  && result.Search.length > 0) {
+        totalResults = result.totalResults;
+        document.getElementById(
+          "result"
+        ).innerText = `About ${result.totalResults} results found`;
+        movies = [...movies, ...result.Search];
+        movieList = result.Search;
+        let resultContainer = document.getElementById("movies");
+        movieList.forEach((movie, idx) => {
+          let movieContainer = document.createElement("div");
+          let containerAttr = document.createAttribute("onclick");
+          containerAttr.value = `redirectToMovie('${movie.imdbID}')`;
+          movieContainer.setAttributeNode(containerAttr);
+          movieContainer.className = "box";
+          let imageHolder = document.createElement("a");
+          imageHolder.classList.add("image");
+          imageHolder.classList.add("fit");
+          let movieImage = document.createElement("img");
+          movieImage.src =
+            movie.Poster && movie.Poster != "N/A"
+              ? movie.Poster
+              : "img/movie.png";
+          imageHolder.appendChild(movieImage);
+          movieContainer.appendChild(imageHolder);
+          let movieDetails = document.createElement("div");
+          movieDetails.className = "inner";
+          let movieName = document.createElement("h3");
+          movieName.innerText = movie.Title;
+          movieDetails.appendChild(movieName);
+          let movieYear = document.createElement("p");
+          movieYear.innerText = movie.Year;
+          movieDetails.appendChild(movieYear);
+          movieContainer.appendChild(movieDetails);
+          resultContainer.appendChild(movieContainer);
+        });
+        if (movies.length !== Number(totalResults)) {
+          page++;
+          document.getElementById("loadMore").classList.remove("hide");
+        } else {
+          document.getElementById("loadMore").classList.add("hide");
+        }
+      } else {
+        document.getElementById("result").innerText = `${result.Error}`;
+      }
     });
 }
 
+function redirectToMovie(movieId) {
+  window.location.href = `/movie.html?movieId=${movieId}`;
+}
